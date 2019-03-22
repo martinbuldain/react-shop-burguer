60da2553258dd5a3df5e72357c0862e8809b8042
BL118067 <leonardo.buldain@icbc.com.ar>
2019-03-22 08:57:40 -0300

    Se crea el state de ingredients y se crea el arreglo dinamico de componentes BurguerIngredient para renderizarlos

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -3,15 +3,27 @@ import classes from './Burguer.module.css';
 import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
 
 /** I return a div because I want to use the Burguer as a wrapper for all the ingredients
- * So I can give width and hight to this Burguer wrapper
+ * So I can give width and hight to this Burguer wrapper.
+ * props.ingredients is a javascript object, so I have to transform to an array to loop throw
+ * The idea with transformedIngredients is to have an array of arrays, where each item of the array
+ * is an ingredient.
+ * For example cheese will be a subarray of 2:
+ *  [<BurguerIngredient key=cheese0 type=cheese />
+ *  <BurguerIngredient key=cheese1 type=cheese />]
  */
 const burguer = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+            return <BurguerIngredient key={ingredientKey + index} type={ingredientKey} />
+        })
+    });
+
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burguer}>
-            <BurguerIngredient type="bread-top"/>
-            <BurguerIngredient type="cheese"/>
-            <BurguerIngredient type="meat"/>
-            <BurguerIngredient type="bread-bottom"/>
+            <BurguerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurguerIngredient type="bread-bottom" />
         </div>
     );
 }
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -3,10 +3,20 @@ import Aux from '../../hoc/Auxiliary';
 import Burguer from '../../components/Burguer/Burguer';
 
 class BurguerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burguer />
+                <Burguer ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
