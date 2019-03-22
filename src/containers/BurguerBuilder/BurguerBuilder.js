06e6d19263c5debf24320e6b6efabc5565759b01
Martin <martinbuldain@gmail.com>
2019-03-22 19:29:21 -0300

    Se agrega la logica para deshabilitar los botones Less si la suma de ingredientes es menor a 0

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -35,17 +35,46 @@ class BurguerBuilder extends Component {
             totalPrice: newPrice
         });
     }
-
+    
+    /**
+     * If oldCount is negative or 0, do nothing
+     * Also I want to disable 'Less' button if oldCount satifies this condition
+     */
     removeIngredientHandler = (type) => {
-        
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredient = {...this.state.ingredients};
+        updatedIngredient[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({
+            ingredients: updatedIngredient,
+            totalPrice: newPrice
+        });
+
     }
 
+    /**
+     * disabledInfo holds a boolean for each ingredient if it is less than 0
+     */
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (const key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return (
             <Aux>
                 <Burguer ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Aux>
         );
     }
