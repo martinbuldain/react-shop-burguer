2462f25a1ec0c9fab98568c0cb9ed13cf65215cb
Martin <martinbuldain@gmail.com>
2019-04-04 19:52:24 -0300

    Se agrega la libreria react-router-dom y se hace la configuracion inicial en App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
 
+
+/**
+ * Switch will match the first route and will stop analizing, but for using this I nee
+ * to change the order.
+ * If I put exact in path="/" the order does not matter, because "/" is not treated like a prefix
+ * So to sum up, with exact the order does not matter, witout it, it matters
+ * 
+ */
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurguerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurguerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
