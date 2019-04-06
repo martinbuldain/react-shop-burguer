44f7fa0937a8de6477fdaf584c7a71b6f7a7f95d
Martin <martinbuldain@gmail.com>
2019-04-06 20:02:46 -0300

    Hago el fetch de las ordenes desde firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for(let key in res.data) {
+          // I am copying all data from Firebase
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        this.setState({
+          orders: fetchedOrders,
+          loading: false 
+        });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -12,4 +41,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
