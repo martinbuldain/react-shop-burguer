42a0c733b3a2d3210116e5a58646dd531f03a64b
Martin <martinbuldain@gmail.com>
2019-04-07 18:59:34 -0300

    Corrijiendo el caso del combo que no tiene validation rules, se la defino vacia

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import Button from "../../../components/UI/Button/Button";
-import classes from "./ContactData.module.css";
-import axios from "../../../axios-orders";
-import Spinner from "../../../components/UI/Spinner/Spinner";
-import Input from "../../../components/UI/Input/Input";
+import React, { Component } from 'react';
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
     orderForm: {
       name: {
-        elementType: "input",
+        elementType: 'input',
         elementConfig: {
-          type: "text",
-          placeholder: "Your Name"
+          type: 'text',
+          placeholder: 'Your Name'
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
@@ -22,12 +22,12 @@ class ContactData extends Component {
         touched: false
       },
       street: {
-        elementType: "input",
+        elementType: 'input',
         elementConfig: {
-          type: "text",
-          placeholder: "Street"
+          type: 'text',
+          placeholder: 'Street'
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
@@ -35,12 +35,12 @@ class ContactData extends Component {
         touched: false
       },
       zipCode: {
-        elementType: "input",
+        elementType: 'input',
         elementConfig: {
-          type: "text",
-          placeholder: "ZIP Code"
+          type: 'text',
+          placeholder: 'ZIP Code'
         },
-        value: "",
+        value: '',
         validation: {
           required: true,
           minLength: 5,
@@ -50,12 +50,12 @@ class ContactData extends Component {
         touched: false
       },
       country: {
-        elementType: "input",
+        elementType: 'input',
         elementConfig: {
-          type: "text",
-          placeholder: "Country"
+          type: 'text',
+          placeholder: 'Country'
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
@@ -63,12 +63,12 @@ class ContactData extends Component {
         touched: false
       },
       email: {
-        elementType: "input",
+        elementType: 'input',
         elementConfig: {
-          type: "email",
-          placeholder: "Your E-mail"
+          type: 'email',
+          placeholder: 'Your E-mail'
         },
-        value: "",
+        value: '',
         validation: {
           required: true
         },
@@ -76,15 +76,16 @@ class ContactData extends Component {
         touched: false
       },
       deliveryMethod: {
-        elementType: "select",
+        elementType: 'select',
         elementConfig: {
           options: [
-            { value: "fastest", displayValue: "Fastest" },
-            { value: "cheapest", displayValue: "Cheapest" }
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
+        validation: {},
         valid: false,
-        value: ""
+        value: ''
       }
     },
     formIsValid: false,
@@ -94,7 +95,7 @@ class ContactData extends Component {
   checkValidity(value, rules) {
     let isValid = true;
     if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
+      isValid = value.trim() !== '' && isValid;
     }
     if (rules.minLength) {
       isValid = value.length >= rules.minLength && isValid;
@@ -120,12 +121,12 @@ class ContactData extends Component {
       orderData: formData
     };
     axios
-      .post("/orders.json", order)
+      .post('/orders.json', order)
       .then(response => {
         this.setState({
           loading: false
         });
-        this.props.history.push("/");
+        this.props.history.push('/');
       })
       .catch(error => {
         this.setState({
