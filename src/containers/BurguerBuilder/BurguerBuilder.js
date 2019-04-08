8929314a63f325aa5ba594061351a5f01053aec4
Martin <martinbuldain@gmail.com>
2019-04-08 19:51:30 -0300

    Refactor del BurguerBuilder, ahora utilizo lo calculado desde el state

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -7,13 +7,8 @@ import OrderSummary from '../../components/Burguer/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
-};
+import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 /**
  * I ended up turning Modal into a class based component in order to be able to implement
@@ -22,168 +17,154 @@ const INGREDIENT_PRICES = {
  */
 
 class BurguerBuilder extends Component {
+  state = {
+    purchaseable: false,
+    purchasing: false, // Para el order button modal
+    loading: false,
+    error: false
+  };
 
-    state = {
-        ingredients: null,
-        totalPrice: 4,
-        purchaseable: false,
-        purchasing: false, // Para el order button modal
-        loading: false,
-        error: false
-    }
+  componentDidMount() {
+    // axios.get('https://react-my-burger-e97c9.firebaseio.com/ingredients.json')
+    //     .then(response => {
+    //         this.setState({
+    //             ingredients: response.data
+    //         })
+    //     })
+    //     .catch(error => {
+    //         this.setState({
+    //             error: true
+    //         })
+    //     });
+  }
 
-    componentDidMount() {
-        // axios.get('https://react-my-burger-e97c9.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({
-        //             ingredients: response.data
-        //         })
-        //     })
-        //     .catch(error => {
-        //         this.setState({
-        //             error: true
-        //         })
-        //     });
-    }
+  /** purchaseable is use to determine if enable or disable Order Button in BuildControls */
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((newSum, el) => {
+        return newSum + el;
+      }, 0);
+    this.setState({
+      purchaseable: sum > 0
+    });
+  }
 
-    /** purchaseable is use to determine if enable or disable Order Button in BuildControls */
-    updatePurchaseState(ingredients) {
-        const sum = Object.keys(ingredients)
-            .map(igKey => {
-                return ingredients[igKey];
-            })
-            .reduce((newSum, el) => {
-                return newSum + el;
-            }, 0);
-        this.setState({
-            purchaseable: sum > 0
-        });
-    }
+  /**
+   * It will be trigger every time we click the Order Button
+   */
+  purchaseHandler = () => {
+    this.setState({
+      purchasing: true
+    });
+  };
 
-    addIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount + 1;
-        const updatedIngredient = { ...this.state.ingredients };
-        updatedIngredient[type] = updatedCount;
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAddition;
-        this.setState({
-            ingredients: updatedIngredient,
-            totalPrice: newPrice
-        });
-        // Due to the way setState works(it takes time to update), it might not update
-        // ingredients when I use ingredients in updatePurchaseState, that is why i have
-        // to send the updatedIngredient to ensure it is the last value
-        this.updatePurchaseState(updatedIngredient);
-    }
+  /**
+   * When user clicks cancel in the Modal
+   */
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  };
 
-    /**
-     * If oldCount is negative or 0, do nothing
-     * Also I want to disable 'Less' button if oldCount satifies this condition
-     */
-    removeIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        if (oldCount <= 0) {
-            return;
-        }
-        const updatedCount = oldCount - 1;
-        const updatedIngredient = { ...this.state.ingredients };
-        updatedIngredient[type] = updatedCount;
-        const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice - priceDeduction;
-        this.setState({
-            ingredients: updatedIngredient,
-            totalPrice: newPrice
-        });
-        // Due to the way setState works(it takes time to update), it might not update
-        // ingredients when I use ingredients in updatePurchaseState, that is why i have
-        // to send the updatedIngredient to ensure it is the last value
-        this.updatePurchaseState(updatedIngredient);
+  /**
+   * Checkout
+   */
+  purchaseContinueHandler = () => {
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i])
+      );
     }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    // Insert a new route into the stack of pages manually
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
+  };
 
-    /**
-     * It will be trigger every time we click the Order Button
-     */
-    purchaseHandler = () => {
-        this.setState({
-            purchasing: true
-        });
+  /**
+   * disabledInfo holds a boolean for each ingredient if it is less than 0, then
+   * I should disable button Less
+   */
+  render() {
+    const disabledInfo = {
+      ...this.props.ings
+    };
+    for (const key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
     }
-
-    /**
-     * When user clicks cancel in the Modal
-     */
-    purchaseCancelHandler = () => {
-        this.setState({
-            purchasing: false
-        })
+    let orderSummary = null;
+    let burguer = this.state.error ? (
+      <p>Ingredients can't be loaded!</p>
+    ) : (
+      <Spinner />
+    );
+    if (this.props.ings) {
+      burguer = (
+        <Aux>
+          <Burguer ingredients={this.props.ings} />
+          <BuildControls
+            ingredientAdded={this.props.onIngredientAdded}
+            ingredientRemoved={this.props.onIngredientRemove}
+            disabled={disabledInfo}
+            price={this.props.price}
+            purchaseable={this.state.purchaseable}
+            ordered={this.purchaseHandler}
+          />
+        </Aux>
+      );
+      orderSummary = (
+        <OrderSummary
+          ingredients={this.props.ings}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinue={this.purchaseContinueHandler}
+          price={this.props.price}
+        />
+      );
+      if (this.state.loading) {
+        orderSummary = <Spinner />;
+      }
     }
-
-    /**
-     * Checkout
-     */
-    purchaseContinueHandler = () => {
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        // Insert a new route into the stack of pages manually
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
-    }
-
-    /**
-     * disabledInfo holds a boolean for each ingredient if it is less than 0, then
-     * I should disable button Less
-     */
-    render() {
-        const disabledInfo = {
-            ...this.state.ingredients
-        };
-        for (const key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0;
-        }
-        let orderSummary = null;
-        let burguer = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
-        if (this.state.ingredients) {
-            burguer = (
-                <Aux>
-                    <Burguer ingredients={this.state.ingredients} />
-                    <BuildControls
-                        ingredientAdded={this.addIngredientHandler}
-                        ingredientRemoved={this.removeIngredientHandler}
-                        disabled={disabledInfo}
-                        price={this.state.totalPrice}
-                        purchaseable={this.state.purchaseable}
-                        ordered={this.purchaseHandler} />
-                </Aux>
-            );
-            orderSummary = <OrderSummary
-                ingredients={this.state.ingredients}
-                purchaseCancelled={this.purchaseCancelHandler}
-                purchaseContinue={this.purchaseContinueHandler}
-                price={this.state.totalPrice} />
-            if (this.state.loading) {
-                orderSummary = <Spinner />
-            }
-        }
-        return (
-            <Aux>
-                <Modal
-                    show={this.state.purchasing}
-                    modalClosed={this.purchaseCancelHandler}>
-                    {orderSummary}
-                </Modal>
-                {burguer}
-            </Aux>
-        );
-    }
+    return (
+      <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          {orderSummary}
+        </Modal>
+        {burguer}
+      </Aux>
+    );
+  }
 }
 
-export default withErrorHandler(BurguerBuilder, axios);
+const matStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIngredientAdded: ignName =>
+      dispatch({ type: actionTypes.ADD_INGREDIENT, ingredientName: ignName }),
+    onIngredientRemove: ignName =>
+      dispatch({ type: actionTypes.REMOVE_INGREDIENT, ingredientName: ignName })
+  };
+};
+
+export default connect(
+  matStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(BurguerBuilder, axios));
