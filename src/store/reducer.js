e8f22787639a00ca5c68f02e1d841c30b46bd083
Martin <martinbuldain@gmail.com>
2019-04-08 18:30:44 -0300

    Instalacion redux y react-redux, ademas de la creacion y confguracion inicial del store

---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+import * as actionTypes from './actions';
+
+/**
+ * I start converting the BurguerBuilder state to Redux
+ */
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+
+}
+
+export default reducer;
