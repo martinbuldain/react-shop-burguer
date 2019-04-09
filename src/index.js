b28f7e7222e83b133a6cdcf9fa9e2458aec32f59
Martin <martinbuldain@gmail.com>
2019-04-09 19:27:07 -0300

    Agrego redux-thunk, un middleware para bloquear un request en redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import burguerBuilderReducer from './store/reducers/burguerBuilder';
+import thunk from 'redux-thunk';
 
-const store = createStore(burguerBuilderReducer);
+const store = createStore(burguerBuilderReducer, applyMiddleware(thunk));
 
 const app = (
   <Provider store={store}>
