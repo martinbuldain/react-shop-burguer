3aa4980f61ac5f99e8e78d2595cbf68be19485a1
Martin <martinbuldain@gmail.com>
2019-04-10 18:54:05 -0300

    Creacion action creators para la order de compra de la hamburguesa

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -121,19 +121,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({
-          loading: false
-        });
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        this.setState({
-          loading: false
-        });
-      });
   };
 
   inputChangedHandler = (event, inputId) => {
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,2 +1,31 @@
 // This will hold the action creators for submitting the form
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
+export const purchaseBurguerSuccess = (id, orderData) => {
+  return {
+    type: actionTypes.PURCHASE_BURGUER_SUCCESS,
+    orderId: id,
+    orderData: orderData
+  };
+};
+
+export const purchaseBurguerFail = error => {
+  return {
+    type: actionTypes.PURCHASE_BURGUER_FAIL,
+    error: error
+  };
+};
+
+export const purchaseBurguerStart = orderData => {
+  return dispatch => {
+    axios
+      .post('/orders.json', orderData)
+      .then(response => {
+        dispatch(purchaseBurguerStart(response.data, orderData));
+      })
+      .catch(error => {
+        dispatch(purchaseBurguerFail(error));
+      });
+  };
+};
